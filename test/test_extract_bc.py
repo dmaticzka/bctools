8e95597f6f32a33db9544bd216169977ed9f05a7
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2015-09-18 15:30:53 +0200

    rename script

--- test/test_extract_bc.py
+++ test/test_extract_bc.py
@@ -12,7 +12,7 @@ datadir_rel = "../../" + datadir
 def test_call_without_parameters():
     "Call tool withouth any additional parameters."
     run = env.run(
-        "../../extract_bc.py",
+        "../../extract_bcs.py",
         expect_error=True
     )
     assert(re.search("too few arguments", run.stderr))
@@ -21,7 +21,7 @@ def test_call_without_parameters():
 def test_illegal_bcpattern():
     "Check if tool reports illegal barcode pattern"
     run = env.run(
-        "../../extract_bc.py",
+        "../../extract_bcs.py",
         datadir_rel + "/reads.fastq",
         "ILLEGAL",
         expect_error=True
@@ -32,7 +32,7 @@ def test_illegal_bcpattern():
 def test_bcpattern_without_bc():
     "Check if tool complains about missing barcode nts in pattern."
     run = env.run(
-        "../../extract_bc.py",
+        "../../extract_bcs.py",
         datadir_rel + "reads.fastq",
         "NNNNNNN",
         expect_error=True
@@ -43,7 +43,7 @@ def test_bcpattern_without_bc():
 def test_positional_args_only():
     "Extract and remove barcodes, print result to stdout."
     run = env.run(
-        "../../extract_bc.py",
+        "../../extract_bcs.py",
         datadir_rel + "reads.fastq",
         "XXXNNXXX",
     )
@@ -57,7 +57,7 @@ def test_positional_args_only():
 def test_writing_fastq_to_file():
     "Extract and remove barcodes, write result to file."
     env.run(
-        "../../extract_bc.py",
+        "../../extract_bcs.py",
         datadir_rel + "reads.fastq",
         "XXXNNXXX",
         "--outfile", "outfile.fastq",
@@ -70,7 +70,7 @@ def test_writing_fastq_to_file():
 def test_writing_bcs_to_file():
     "Extract and remove barcodes, write extracted barcodes to separate fastq file."
     env.run(
-        "../../extract_bc.py",
+        "../../extract_bcs.py",
         datadir_rel + "reads.fastq",
         "XXXNNXXX",
         "--bcs", "extracted_bcs.fa",
