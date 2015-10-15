41906e446ddeb5c363ebc935f11c57ed050ff835
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2015-10-15 14:03:47 +0200

    add remove_tail.py

---
+++ test/test_remove_tail.py
@@ -0,0 +1,58 @@
+import re
+from filecmp import cmp
+from scripttest import TestFileEnvironment
+
+bindir = "bin/"
+datadir = "test/data/"
+testdir = "test/testenv_remove_tail/"
+env = TestFileEnvironment(testdir)
+# relative to test file environment
+bindir_rel = "../../" + bindir
+datadir_rel = "../../" + datadir
+
+
+def test_call_without_parameters():
+    "Call remove_tail.py withouth any additional parameters."
+    run = env.run(
+        bindir_rel + "remove_tail.py",
+        expect_error=True
+    )
+    assert(re.search("too few arguments", run.stderr))
+
+
+def test_invalid_length():
+    "Call remove_tail.py with negative length."
+    run = env.run(
+        bindir_rel + "remove_tail.py",
+        datadir_rel + "reads.fastq",
+        -7,
+        expect_error=True,
+    )
+    assert(run.returncode != 0)
+
+
+def test_positional_args_only():
+    "Remove nts from 3' tail, print result to stdout."
+    run = env.run(
+        bindir_rel + "remove_tail.py",
+        datadir_rel + "readswithtail.fastq",
+        7,
+    )
+    with open(testdir + "stdout_only_positional_args.fastq", "w") as b:
+        b.write(run.stdout)
+    assert(cmp(
+        testdir + "stdout_only_positional_args.fastq",
+        datadir + "readswithtailremoved.fastq"))
+
+
+def test_writing_fastq_to_file():
+    "Remove nts from 3' tail, write result to file."
+    env.run(
+        bindir_rel + "remove_tail.py",
+        datadir_rel + "readswithtail.fastq",
+        7,
+        "--outfile", "outfile.fastq",
+    )
+    assert(cmp(
+        testdir + "outfile.fastq",
+        datadir + "readswithtailremoved.fastq"))
